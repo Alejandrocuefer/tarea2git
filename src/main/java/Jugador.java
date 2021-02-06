7c6f34047faf4bcb647ac780ec15d13e493fbcc4
AlexC <alexcuevasfer@gmail.com>
2021-02-06 19:24:28 +0100

    Getter setter y constructor

--- src/main/java/Jugador.java
+++ src/main/java/Jugador.java
@@ -9,7 +9,39 @@
  * @author a
  */
 public class Jugador {
-    String nombre;
-    int votos;
-    int id;
+    private String nombre;
+    private int votos;
+    private int id;
+
+    public Jugador(String nombre, int votos, int id) {
+        this.nombre = nombre;
+        this.votos = votos;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+    
+    
 }
